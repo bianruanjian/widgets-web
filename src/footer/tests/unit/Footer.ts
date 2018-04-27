6b0bb4c2b375b601a962f63aabe0d635cb60da15
jfx <jiangfx@iri.cn>
2018-04-27 10:12:55 +0800

    develop the web version of Footer widget

---
+++ src/footer/tests/unit/Footer.ts
@@ -0,0 +1,77 @@
+const { describe, it} = intern.getInterface('bdd');
+
+import { w, v } from '@dojo/widget-core/d';
+import harness from '@dojo/test-extras/harness';
+
+import Footer, { FooterProperties } from './../../index';
+
+describe('Footer', () => {
+	const defaultProperties: FooterProperties = {
+		marginTop: "default",
+		marginBottom: "default",
+		marginLeft: "default",
+		marginRight: "default",
+		paddingTop: "default",
+		paddingBottom: "default",
+		paddingLeft: "default",
+		paddingRight: "default",
+		flexDirection: "default",
+		reverse: false,
+		justifyItems: "default",
+		alignItems: "default",
+		flexWrap: "default",
+		alignContent: "default"
+	};
+
+	const customProperties: FooterProperties = {
+		widgetId: "random-id",
+		marginTop: "0",
+		marginBottom: "0",
+		marginLeft: "1",
+		marginRight: "1",
+		paddingTop: "0",
+		paddingBottom: "0",
+		paddingLeft: "1",
+		paddingRight: "1",
+		flexDirection: "row",
+		reverse: true,
+		justifyItems: "start",
+		alignItems: "start",
+		flexWrap: "nowrap",
+		alignContent: "start"
+	};
+
+	it('should construct Footer', () => {
+		const h = harness(() => w(Footer, {}));
+		h.expect(() => v('div', {
+			id: undefined,
+			classes: []
+		}, []));
+	});
+
+	it('default properties', () => {
+		const h = harness(() => w(Footer, defaultProperties));
+		h.expect(() => v('div', {
+			id: undefined,
+			classes: []
+		}, []));
+	});
+
+	it('custom properties', () => {
+		const h = harness(() => w(Footer, customProperties));
+		h.expect(() => v('div', {
+			id: 'random-id',
+			classes: [
+				'my-0',
+				'mx-1',
+				'py-0',
+				'px-1',
+				'flex-row-reverse',
+				'justify-content-start',
+				'align-items-start',
+				'flex-nowrap',
+				'align-content-start'
+			]
+		}, []));
+	});
+});
