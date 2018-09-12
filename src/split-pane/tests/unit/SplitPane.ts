2a5ad1b42530e4a1bdd5469a5483b89cbbc7626b
jfx <jiangfx@iri.cn>
2018-09-12 09:41:47 +0800

    #631 support SplitPane widget of dojo

---
+++ src/split-pane/tests/unit/SplitPane.ts
@@ -0,0 +1,19 @@
+const { describe, it } = intern.getInterface('bdd');
+
+import harness from '@dojo/framework/testing/harness';
+import SplitPane from './../../index';
+import { w } from '@dojo/framework/widget-core/d';
+import { SplitPaneBase } from '@dojo/widgets/split-pane';
+
+describe('SplitPane', () => {
+	it('should construct SplitPane', () => {
+		const h = harness(() => w(SplitPane, {}));
+		h.expect(() =>
+			w(SplitPaneBase, {
+				size: 100,
+				onResize: () => {},
+				onCollapse: () => {}
+			})
+		);
+	});
+});
